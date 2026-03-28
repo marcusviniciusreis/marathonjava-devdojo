9d56b16ef480ba34d712f25ae60ceee443e9acda
Marcus Vinícius <marcus21.dev@gmail.com>
2026-03-28 16:16:41 -0300

    feat: implement methods in OOP

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for( double salario: salarios){
+            System.out.print(salario+ " ");
+        }
+        imprimeMediaSalario();
+
+    }
+    public void imprimeMediaSalario(){
+        double media = 0;
+        for (double salario: salarios){
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMedia salarial "+media);
+    }
+
+}
