d22454e6f633e8e841a287175f68885fa7a79e19
Marcus Vinícius <marcus21.dev@gmail.com>
2026-03-29 18:53:47 -0300

    feat: add encapsulation with private, getters and setters

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime(){
         System.out.println(this.nome);
@@ -15,12 +16,39 @@ public class Funcionario {
 
     }
     public void imprimeMediaSalario(){
-        double media = 0;
         for (double salario: salarios){
             media += salario;
         }
         media /= salarios.length;
         System.out.println("\nMedia salarial "+media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
     }
 
 }
