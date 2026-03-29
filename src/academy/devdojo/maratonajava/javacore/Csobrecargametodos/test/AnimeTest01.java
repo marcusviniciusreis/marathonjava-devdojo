c22084f8986bc5bb06c1d4ba20638456d2bd20d7
Marcus Vinícius <marcus21.dev@gmail.com>
2026-03-29 19:12:58 -0300

    feat: add method overloading

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("SakamotoDays", "TV", 20, "Ação");
+        anime.setGenero("Ação");
+        anime.imprime();
+    }
+}
