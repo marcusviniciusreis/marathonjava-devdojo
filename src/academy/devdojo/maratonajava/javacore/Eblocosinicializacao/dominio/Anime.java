0ecca749cda62573cb9404620b918d748c0d41de
Marcus Vinícius <marcus21.dev@gmail.com>
2026-03-29 20:03:55 -0300

    feat: add initialization blocks

---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    // 1 - alocado espaço em memoria
+    // 2 - cada atributo de classe é criado e inicializado com valores default ou o que for passado
+    // 3 - Bloco de inicialização é executado
+    // 4 - Contrustor é executado
+    {
+        System.out.println("Dentro do bloco de inicializaçao");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for(int episodio: this.episodios){
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
