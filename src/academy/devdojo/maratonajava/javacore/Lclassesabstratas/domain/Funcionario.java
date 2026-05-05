0122705364416699c00d35fad49ef50d1a74dba8
Marcus Vinícius <marcus21.dev@gmail.com>
2026-05-04 21:15:51 -0300

    feat: learning abstract classes and usage

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/domain/Funcionario.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.domain;
+
+public abstract class Funcionario extends Pessoa{
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+        calculaBonus();
+    }
+
+    public abstract void calculaBonus();
+
+}
