b728ed0ab4150994fced16f5582d14619c46634d
Marcus Vinícius <marcus21.dev@gmail.com>
2026-06-14 16:28:25 -0300

    learn: study LocalDate, LocalTime and LocalDateTime for modern date-time representation

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2022-08-06");
+        LocalTime time = LocalTime.parse("23:45:00");
+        System.out.println(localDateTime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime localDateTime1 =  date.atTime(time);
+        LocalDateTime localDateTime2 = time.atDate(date);
+        System.out.println(localDateTime1);
+        System.out.println(localDateTime2);
+
+    }
+}
