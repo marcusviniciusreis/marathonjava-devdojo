68aac6dfa2fe1b41dea802829599624b1775b567
Marcus Vinícius <marcus21.dev@gmail.com>
2026-06-18 11:46:57 -0300

    learn: study Serialization for object persistence with ObjectOutputStream and ObjectInputStream

---
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.Xserializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Xserializacao.domain.Aluno;
+import academy.devdojo.maratonajava.javacore.Xserializacao.domain.Turma;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "William Suane", "123123123");
+        Turma turma = new Turma("Maratona Java Viradao no Jiraya em breve ricos");
+        aluno.setTurma(turma);
+        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private static void deserializar() {
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))){
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
