269d4bca0037e67273e894bb49444e50a3d542a4
Marcus Vinícius <marcus21.dev@gmail.com>
2026-07-06 19:22:06 -0300

    learn: study Set, HashSet, NavigableSet and TreeSet for unique element collections

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableTest01.java
@@ -0,0 +1,54 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Manga;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Smartphone;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+
+    @Override
+    public int compare(Smartphone o1, Smartphone o2) {
+        return o1.getMarca().compareTo(o2.getMarca());
+    }
+}
+
+class MangaPrecoComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
+public class NavigableTest01 {
+    public static void main(String[] args) {
+        NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
+        Smartphone smartphone = new Smartphone("123", "Nokia");
+        set.add(smartphone);
+
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
+        mangas.add(new Manga(5L,"Hellsing Ultimate", 19.9, 5));
+        mangas.add(new Manga(3L,"Attack on titan",9.5, 0));
+        mangas.add(new Manga(1L,"Berserk",3.2, 2));
+        mangas.add(new Manga(4L,"Pokemon",11.20,0));
+        mangas.add(new Manga(2L,"DBZ",2.99,0));
+        mangas.add(new Manga(10L,"Aaragon",2.99,0));
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        Manga yuyu = new Manga(21L, "Attack on titan", 3.2, 0);
+        System.out.println("------------------");
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
+
+    }
+}
--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -2,27 +2,23 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Manga;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
-public class IteratorTest01 {
+public class SetTest01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new LinkedList<>();
+        Set<Manga> mangas = new HashSet<>();
         mangas.add(new Manga(5L,"Hellsing Ultimate", 19.9, 5));
         mangas.add(new Manga(3L,"Attack on titan",9.5, 0));
         mangas.add(new Manga(1L,"Berserk",3.2, 2));
         mangas.add(new Manga(4L,"Pokemon",11.20,0));
         mangas.add(new Manga(2L,"DBZ",2.99,0));
 
-//        Iterator<Manga> mangaIterator = mangas.iterator();
-//        while (mangaIterator.hasNext()){
-//            if (mangaIterator.next().getQuantidade()==0){
-//                mangaIterator.remove();
-//            }
-//        }
-        mangas.removeIf(manga -> manga.getQuantidade() == 0);
-        System.out.println(mangas);
+        for (Manga m: mangas){
+            System.out.println(m);
+        }
+
     }
 }
