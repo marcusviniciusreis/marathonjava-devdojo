e87d3988bf53cb293707f051528fc01639a51fc3
Marcus Vinícius <marcus21.dev@gmail.com>
2026-07-09 09:59:17 -0300

    learn: study Method References for static, instance and constructor references

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.domain.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReferenceTest01 {
+    public static void main(String[] args) {
+        List<Anime> animeList = new ArrayList<>(List.of(
+                new Anime("Berserk", 43),
+                new Anime("One Piece", 900),
+                new Anime("Naruto", 500)));
+
+//        Collections.sort(animeList, (a1,a2) -> a1.getTitle().compareTo(a2.getTitle()));
+//        Collections.sort(animeList, AnimeComparators::compareByTitle);
+        animeList.sort(AnimeComparators::compareByEpisodes);
+        System.out.println(animeList);
+
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.domain.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(
+                new Anime("Berserk", 43),
+                new Anime("One Piece", 900),
+                new Anime("Naruto", 500)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+    }
+}
